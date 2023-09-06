5cd75086260376953ac86c121ed1d2189e7513c4
hlfan3 <hlfan3@iflytek.com>
2023-09-06 18:53:51 +0800

    fix: 分库

--- sub-db-table/src/main/java/com/oujiong/Application.java
+++ sub-db-table/src/main/java/com/oujiong/Application.java
@@ -1,21 +1,21 @@
 package com.oujiong;
 
-import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Description: 启动类
- *
- * TODO 这里一定要排除这里的SpringBootConfiguration，因为我们已经自定义了DataSource，所以需要排序shardingjdbc设置的DataSource
  * @author xub
+ * @Description: 启动类
+ * <p>
+ * TODO 这里一定要排除这里的SpringBootConfiguration，因为我们已经自定义了DataSource，所以需要排序shardingjdbc设置的DataSource
  * @date 2019/10/08 下午6:33
  */
-@SpringBootApplication(exclude = SpringBootConfiguration.class)
+//@SpringBootApplication(exclude = SpringBootConfiguration.class)
+@SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
